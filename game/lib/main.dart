760f6960480bcbfafa418d6d51c7277cf4a82220
Marlon Passos <101027484+MarlonVBP@users.noreply.github.com>
2025-11-09 14:40:41 -0300

    :sparkles: :feat Salvando os dados no firebase

--- game/lib/main.dart
+++ game/lib/main.dart
@@ -2,8 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:teste/providers/game_state.dart';
 import 'package:teste/screens/home_screen.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:teste/firebase_options.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/foundation.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  if (!kIsWeb) {
+    FirebaseDatabase.instance.setPersistenceEnabled(true);
+  }
+
   runApp(
     ChangeNotifierProvider(
       create: (context) => GameState(),
--- game/lib/providers/game_state.dart
+++ game/lib/providers/game_state.dart
@@ -4,7 +4,9 @@ import 'package:teste/models/character_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:teste/models/item_model.dart';
 import 'package:teste/models/quest_model.dart';
+import 'package:firebase_database/firebase_database.dart';
 
+//
 final List<HeroCharacter> defaultHeroes = [
   HeroCharacter(
     name: 'Guerreiro velho',
@@ -19,17 +21,55 @@ final List<HeroCharacter> defaultHeroes = [
       heroClass: 'Ladino'),
 ];
 
+//
+//
+const List<GameItem> allGameItems = [
+  GameItem(
+      name: 'Poção de Cura',
+      description: 'Restaura 50 HP',
+      type: ItemType.potion,
+      imagePath: 'images/health_potion.png',
+      healAmount: 50,
+      price: 10),
+  GameItem(
+      name: 'Espada Curta',
+      description: '+5 de Ataque',
+      type: ItemType.sword,
+      imagePath: 'images/small_sword.png',
+      attackBonus: 5,
+      price: 100),
+  GameItem(
+      name: 'Armadura leve',
+      description: '+5 de Defesa',
+      type: ItemType.armor,
+      imagePath: 'images/armor_leather.png',
+      defenseBonus: 5,
+      price: 80),
+  GameItem(
+      name: 'Cajado Místico',
+      description: '+10 Ataque Mágico',
+      type: ItemType.staff,
+      imagePath: 'images/magic_staff.png',
+      price: 150),
+];
+
 class GameState with ChangeNotifier {
+  final DatabaseReference _dbRef = FirebaseDatabase.instance.ref();
+
   HeroCharacter? selectedHero;
   String? selectedScenario;
   List<GameItem> playerInventory = [];
   List<Quest> quests = [];
-
+  int playerGold = 0;
   List<HeroCharacter> availableHeroes = [];
+
+  final String _userId = "testUser123";
 
   GameState() {
     _initializeQuests();
+
     loadHeroes();
+    _loadGameData();
   }
 
   void _initializeQuests() {
@@ -65,26 +105,172 @@ class GameState with ChangeNotifier {
     ];
   }
 
+  void _loadGameData() {
+    _dbRef.child('users/$_userId').onValue.listen((event) {
+      final data = event.snapshot.value;
+
+      if (data != null && data is Map) {
+        final userData = Map<String, dynamic>.from(data);
+
+        playerGold = userData['playerGold'] as int? ?? 0;
+
+        final List<dynamic>? inventoryNames =
+            userData['playerInventory'] as List<dynamic>?;
+        playerInventory.clear();
+        if (inventoryNames != null) {
+          for (final itemName in inventoryNames) {
+            try {
+              final item =
+                  allGameItems.firstWhere((item) => item.name == itemName);
+              playerInventory.add(item);
+            } catch (e) {
+              if (kDebugMode) {
+                print("Item $itemName não encontrado na lista mestre.");
+              }
+            }
+          }
+        }
+
+        final Map<dynamic, dynamic>? questStatus =
+            userData['questStatus'] as Map<dynamic, dynamic>?;
+        if (questStatus != null) {
+          for (final quest in quests) {
+            quest.isCompleted = questStatus[quest.id] as bool? ?? false;
+          }
+        }
+      }
+
+      notifyListeners();
+    }, onError: (error) {
+      if (kDebugMode) {
+        print("Erro ao carregar dados do jogo: $error");
+      }
+    });
+  }
+
+  Future<void> loadHeroes() async {
+    final prefs = await SharedPreferences.getInstance();
+    try {
+      _dbRef.child('users/$_userId/heroes').onValue.listen((event) {
+        final data = event.snapshot.value;
+        if (data != null && data is Map) {
+          final heroesMap = Map<String, dynamic>.from(data);
+          availableHeroes = heroesMap.values.map((heroData) {
+            return HeroCharacter.fromJson(Map<String, dynamic>.from(heroData));
+          }).toList();
+        } else {
+          availableHeroes = List.from(defaultHeroes);
+
+          _saveHeroesToFirebase();
+        }
+
+        final String? lastSelectedHeroName =
+            prefs.getString('last_selected_hero');
+        if (lastSelectedHeroName != null) {
+          try {
+            selectedHero = availableHeroes
+                .firstWhere((h) => h.name == lastSelectedHeroName);
+          } catch (e) {
+            selectedHero = null;
+          }
+        }
+        notifyListeners();
+      });
+    } catch (e) {
+      if (kDebugMode) {
+        print("Erro ao carregar heróis do Firebase: $e");
+      }
+
+      availableHeroes = List.from(defaultHeroes);
+      notifyListeners();
+    }
+  }
+
+  Future<void> _saveHeroesToFirebase() async {
+    Map<String, dynamic> heroesMap = {
+      for (var hero in availableHeroes) hero.name: hero.toJson()
+    };
+    try {
+      await _dbRef.child('users/$_userId/heroes').set(heroesMap);
+    } catch (e) {
+      if (kDebugMode) {
+        print("Erro ao salvar heróis no Firebase: $e");
+      }
+    }
+  }
+
+  Future<void> _updateHeroInFirebase(HeroCharacter hero) async {
+    try {
+      await _dbRef
+          .child('users/$_userId/heroes/${hero.name}')
+          .update(hero.toJson());
+    } catch (e) {
+      if (kDebugMode) {
+        print("Erro ao atualizar herói no Firebase: $e");
+      }
+    }
+  }
+
+  Future<void> _addHeroToFirebase(HeroCharacter hero) async {
+    try {
+      await _dbRef
+          .child('users/$_userId/heroes/${hero.name}')
+          .set(hero.toJson());
+    } catch (e) {
+      if (kDebugMode) {
+        print("Erro ao adicionar herói no Firebase: $e");
+      }
+    }
+  }
+
+  Future<void> _savePlayerInventory() async {
+    List<String> itemNames = playerInventory.map((item) => item.name).toList();
+    try {
+      await _dbRef.child('users/$_userId/playerInventory').set(itemNames);
+    } catch (e) {
+      if (kDebugMode) {
+        print("Erro ao salvar inventário no Firebase: $e");
+      }
+    }
+  }
+
+  Future<void> _savePlayerGold() async {
+    try {
+      await _dbRef.child('users/$_userId/playerGold').set(playerGold);
+    } catch (e) {
+      if (kDebugMode) {
+        print("Erro ao salvar ouro no Firebase: $e");
+      }
+    }
+  }
+
+  Future<void> _saveQuestStatus(String questId, bool isCompleted) async {
+    try {
+      await _dbRef
+          .child('users/$_userId/questStatus')
+          .update({questId: isCompleted});
+    } catch (e) {
+      if (kDebugMode) {
+        print("Erro ao salvar status da missão: $e");
+      }
+    }
+  }
+
   void completeQuest(String questId) {
     final quest = quests.firstWhere((q) => q.id == questId);
     quest.isCompleted = true;
+    _saveQuestStatus(questId, true);
     notifyListeners();
   }
 
-  int playerGold = 0;
-
   void addGold(int amount) {
     playerGold += amount;
+    _savePlayerGold();
     notifyListeners();
   }
 
   void selectAndSaveHero(HeroCharacter hero) {
-    selectedHero = hero;
-    if (!availableHeroes.any((h) => h.name == hero.name)) {
-      availableHeroes.add(hero);
-      _saveHeroes();
-    }
-    notifyListeners();
+    selectHero(hero);
   }
 
   void updateHero(HeroCharacter updatedHero) {
@@ -96,51 +282,24 @@ class GameState with ChangeNotifier {
         selectedHero = updatedHero;
       }
 
-      _saveHeroes();
+      _updateHeroInFirebase(updatedHero);
       notifyListeners();
     }
   }
 
-  Future<void> _saveHeroes() async {
-    final prefs = await SharedPreferences.getInstance();
-    List<String> heroesJsonList =
-        availableHeroes.map((h) => jsonEncode(h.toJson())).toList();
-    await prefs.setStringList('saved_heroes_list', heroesJsonList);
-  }
-
-  Future<void> loadHeroes() async {
-    final prefs = await SharedPreferences.getInstance();
-    final List<String>? heroesJsonList =
-        prefs.getStringList('saved_heroes_list');
-
-    if (heroesJsonList != null && heroesJsonList.isNotEmpty) {
-      availableHeroes = heroesJsonList
-          .map((hJson) => HeroCharacter.fromJson(jsonDecode(hJson)))
-          .toList();
-    } else {
-      availableHeroes = List.from(defaultHeroes);
-    }
-
-    final String? lastSelectedHeroName = prefs.getString('last_selected_hero');
-    if (lastSelectedHeroName != null) {
-      try {
-        selectedHero =
-            availableHeroes.firstWhere((h) => h.name == lastSelectedHeroName);
-      } catch (e) {
-        selectedHero = null;
-      }
-    }
-    notifyListeners();
-  }
-
-  void selectHero(HeroCharacter hero) {
+  void selectHero(HeroCharacter hero) async {
     selectedHero = hero;
 
-    if (!availableHeroes.any((h) => h.name == hero.name)) {
+    bool heroExists = availableHeroes.any((h) => h.name == hero.name);
+
+    if (!heroExists) {
       availableHeroes.add(hero);
+      await _addHeroToFirebase(hero);
     }
 
-    _saveHeroes();
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('last_selected_hero', hero.name);
+
     notifyListeners();
   }
 
@@ -152,17 +311,20 @@ class GameState with ChangeNotifier {
   void addItemToInventory(GameItem item) {
     if (playerInventory.length < 5) {
       playerInventory.add(item);
+      _savePlayerInventory();
       notifyListeners();
     }
   }
 
   void removeItemFromInventory(GameItem item) {
     playerInventory.remove(item);
+    _savePlayerInventory();
     notifyListeners();
   }
 
   void clearInventory() {
     playerInventory.clear();
+    _savePlayerInventory();
     notifyListeners();
   }
 }
